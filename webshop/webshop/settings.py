8b5b90f6738a18f77fa9b5f464cd5e6385b63bd0
netmaster81 <61751225+netmaster81@users.noreply.github.com>
2022-06-08 15:43:29 +0200

    Create app&model&serializer

--- webshop/webshop/settings.py
+++ webshop/webshop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'djoser',
+    'product',
 ]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080"
@@ -125,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
